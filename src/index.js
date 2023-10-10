903c98988fc1ae97855ce4152d89763a01f157f6
MokwaTheDeveloper <moffatmokwa12@gmail.com>
2023-10-10 12:50:51 +0300

    Completed Assigment

--- src/index.js
+++ src/index.js
@@ -13,3 +13,41 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+fetch("http://localhost:3000/toys", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    "name": "Jessie",
+    "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+    "likes": 0
+  })
+})
+  .then(response => response.json())
+  .then(data => {
+    // Handle the response data
+  })
+  .catch(error => {
+    // Handle the error
+  });
+  
+fetch("http://localhost:3000/toys/:id", {
+  method: "PATCH",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    "likes": newNumberOfLikes
+  })
+})
+  .then(response => response.json())
+  .then(data => {
+    // Update the toy's card in the DOM based on the response data
+  })
+  .catch(error => {
+    // Handle the error
+  });
